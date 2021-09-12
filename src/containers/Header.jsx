21e7f2f2b61223d26e0946f243656202f3c62776
Chikanma <chikanmaonuegbu@gmail.com>
2021-09-12 16:40:42 -0600

    mfsn

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Header = () =>{
-    return(
-        <div >
-
-        </div>
-        )
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>FakeShop</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
